bf8485bf6b46f21bb7d6a6e61934192ff8eb7529
Nicholas Alderman <nicholasaalderman19@gmail.com>
2022-10-13 17:02:23 -0400

    added audio recording functionality but need a better way to save audio data to db

---
+++ 2/2.6/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const Datastore = require('nedb');
+
+const app = express();
+app.listen(5050, () => console.log("Listening at 5050"));
+
+const db = new Datastore('database.db');
+db.loadDatabase();
+
+app.use(express.static('public'));
+app.use(express.json());
+
+app.post('/api', (request, response) => {
+    const data = request.body;
+    db.insert(data);
+    
+    response.json({
+        status: "sent"
+    });
+});
+
+app.post('/db', (request, response) => {
+    const kw = request.body.searchWord;
+    db.find({word: kw}, (err, docs) => {
+        if (err) {
+            response.send('bad search');
+        } else {
+            response.json(docs);
+        }
+    });
+});
+
+app.post('/audioURL', (request, response) => {
+    // const data = request.body;
+    // db.insert(data);
+    console.log(request);
+
+    response.json({
+        status: "sent"
+    });
+});
