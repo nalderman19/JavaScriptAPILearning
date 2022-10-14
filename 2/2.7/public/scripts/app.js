3b1d3201e5ca73ab545c593b93f2fbfa100fa488
Nicholas Alderman <nicholasaalderman19@gmail.com>
2022-10-13 20:28:51 -0400

    can now save audio recording as a base64 encoding and save to db

---
+++ 2/2.7/public/scripts/app.js
@@ -0,0 +1,97 @@
+// // set up basic variables for app
+
+// const record = document.querySelector('.record');
+// const stop = document.querySelector('.stop');
+// const soundClips = document.querySelector('.sound-clips');
+// const mainSection = document.querySelector('.main-controls');
+
+// // disable stop button while not recording
+
+// stop.disabled = true;
+
+// //main block for doing the audio recording
+
+// if (navigator.mediaDevices.getUserMedia) {
+//   console.log('getUserMedia supported.');
+
+//   const constraints = { audio: true };
+//   let chunks = [];
+
+//   let onSuccess = function(stream) {
+//     const mediaRecorder = new MediaRecorder(stream);
+
+//     record.onclick = function() {
+//       mediaRecorder.start();
+//       console.log(mediaRecorder.state);
+//       console.log("recorder started");
+//       record.style.background = "red";
+
+//       stop.disabled = false;
+//       record.disabled = true;
+//     }
+
+//     stop.onclick = function() {
+//       mediaRecorder.stop();
+//       console.log(mediaRecorder.state);
+//       console.log("recorder stopped");
+//       record.style.background = "";
+//       record.style.color = "";
+//       // mediaRecorder.requestData();
+
+//       stop.disabled = true;
+//       record.disabled = false;
+//     }
+
+//     mediaRecorder.onstop = function(e) {
+//       console.log("data available after MediaRecorder.stop() called.");
+
+//       const clipContainer = document.createElement('article');
+//       const clipLabel = document.createElement('p');
+//       const audio = document.createElement('audio');
+      
+
+//       clipContainer.classList.add('clip');
+//       audio.setAttribute('controls', '');
+
+//       const clipName = "clip_001"
+//       clipLabel.textContent = clipName;
+
+//       soundClips.appendChild(clipContainer);
+
+//       audio.controls = true;
+//       const blob = new Blob(chunks, { 'type' : 'audio/ogg; codecs=opus' });
+//       chunks = [];
+//       const audioURL = window.URL.createObjectURL(blob);
+//       audio.src = audioURL;
+//       console.log("recorder stopped");
+
+      
+//       sendURL(audioURL);
+//     }
+
+//     mediaRecorder.ondataavailable = function(e) {
+//       chunks.push(e.data);
+//     }
+//   }
+
+//   let onError = function(err) {
+//     console.log('The following error occured: ' + err);
+//   }
+
+//   navigator.mediaDevices.getUserMedia(constraints).then(onSuccess, onError);
+
+// } else {
+//    console.log('getUserMedia not supported on your browser!');
+// }
+
+// async function sendURL(url) {
+//   const data = {  };
+//   const options = {
+//     method: 'POST',
+//     headers: {
+//     'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify(data),
+//   };
+//   const response = await fetch('/audioURL', options);
+// }
